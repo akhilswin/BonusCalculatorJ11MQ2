1632886d96ef856c5a01f3d4444b8ed4872f3c5d
Akhilesh <Akhilesh@Akhilesh-PC>
2020-09-08 13:13:36 +0530

    BonusCalculatorJ11MQ2

---
+++ BonusCalculatorJ11MQ2/src/test/java/yaksha/MainClassTest.java
@@ -0,0 +1,29 @@
+package yaksha;
+
+import static yaksha.TestUtils.businessTestFile;
+import static yaksha.TestUtils.currentTest;
+import static yaksha.TestUtils.yakshaAssert;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+import org.junit.jupiter.api.Test;
+
+class MainClassTest {
+	@Test
+	void testCalculateBonus() throws Exception {
+		// Test will pass
+		HashMap<Integer, String> hm = new HashMap<Integer, String>();
+		HashMap<Integer, Integer> hm1 = new HashMap<Integer, Integer>();
+		TreeMap<Integer, Integer> hm2 = new TreeMap<Integer, Integer>();
+		hm2.put(1010, 12000);
+		hm2.put(2020, 17280);
+		hm.put(1010, "20-12-1987");
+		hm1.put(2020, 14400);
+		String expectedResult[] = { "1010=12000", "2020=17280" };
+		List<String> list1 = Arrays.asList(expectedResult);
+		TreeMap<Integer, Integer> result3 = BonusCalculator.calculateBonus(hm, hm1);
+		var list2 = Arrays.asList(result3);
+		yakshaAssert(currentTest(), (list1.containsAll(list2) ? "true" : "false"), businessTestFile);
+	}
+}
