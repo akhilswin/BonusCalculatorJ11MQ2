5e6430735391b962d1401c04d7685f0dbeb7e212
Akhilesh <Akhilesh@Akhilesh-PC>
2020-09-12 11:49:12 +0530

    BonusCalculatorJ11MQ2

--- BonusCalculatorJ11MQ2/src/main/java/yaksha/MainClass.java
+++ BonusCalculatorJ11MQ2/src/main/java/yaksha/MainClass.java
@@ -11,7 +11,9 @@ public class MainClass {
 		HashMap<Integer, Integer> hm1 = new HashMap<Integer, Integer>();
 		TreeMap<Integer, Integer> hm2 = new TreeMap<Integer, Integer>();
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number of Employees:");
 		int s = Integer.parseInt(sc.nextLine());
+		System.out.println("Enter Details:");
 		for (int i = 0; i < s; i++) {
 			int id = Integer.parseInt(sc.nextLine());
 			String ss = sc.nextLine();
@@ -22,6 +24,7 @@ public class MainClass {
 		}
 		hm2 = BonusCalculator.calculateBonus(hm, hm1);
 		Iterator<Integer> it = hm2.keySet().iterator();
+		System.out.println("Result is:");
 		for (int i = 0; i < s; i++) {
 			int id = it.next();
 			int bonus = hm2.get(id);
--- BonusCalculatorJ11MQ2/src/test/java/yaksha/MainClassTest.java
+++ BonusCalculatorJ11MQ2/src/test/java/yaksha/MainClassTest.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
 
 class MainClassTest {
 
